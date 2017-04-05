c6c3751325854f3f3003e156b1dff6eb7c38417d
Punit Naik <naik.punit44@gmail.com>
2017-04-05 10:25:55 +0530

    part two

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -21,27 +21,45 @@
     ; Movement in the North direction: X,+Y
     (or (and (= current-orientation "E") (= direction "L"))
         (and (= current-orientation "W") (= direction "R")))
-      ["N" current-x (+ current-y distance)]))
+      ["N" current-x (+ current-y distance)]
+    ; Other cases for no direction; Just moving forward in the current orientation
+    (and (= direction "") (= current-orientation "E"))
+      [current-orientation (+ current-x distance) current-y]
+    (and (= direction "") (= current-orientation "W"))
+      [current-orientation (- current-x distance) current-y]
+    (and (= direction "") (= current-orientation "S"))
+      [current-orientation current-x (- current-y distance)]
+    (and (= direction "") (= current-orientation "N"))
+      [current-orientation current-x (+ current-y distance)]))
     
 (defn split-direction-and-distance
   "Splits direction and distance from a string by applying regex"
   [s]
   (let [[[original-string direction distance]] (re-seq #"(R|L)(\d+)" s)]
-    [direction (Integer/parseInt distance)]))
+    (->> (take (dec (Integer/parseInt distance)) (repeat 1)) ; Moving point by point
+         (map (fn [x] ["" x]))
+         (cons [direction 1])))) ; Prepending the original direction
 
 (defn read-instructions
   "Reads instructions from file"  
   [instructions-file]
   (->> (split (slurp instructions-file) #",\s")
-       (mapv split-direction-and-distance)))
+       (mapv split-direction-and-distance)
+       (reduce concat)))
       
 (defn calculate-distance
   "Calculates distance of the Easter Bunny from the headquarters"
-  [instructions [orientation x y :as orientation-location]]
+  [instructions [orientation x y :as orientation-location] location-tracker]
   (if-let [[dir dist] (first instructions)]
     ; Recursively iterate
-    (->> (move-on-the-2d-plane orientation-location dir dist)
-         (calculate-distance (drop 1 instructions)))
+    (let [[new-orientation new-x new-y :as new-orient-loc] (move-on-the-2d-plane orientation-location dir dist)]
+      (if (@location-tracker [new-x new-y])
+        ; If already visited, return distance
+        (+ (Math/abs new-x) (Math/abs new-y))
+        ; Else update location tracker and continue the recursion
+        (do
+          (swap! location-tracker assoc [new-x new-y] 0)
+          (calculate-distance (drop 1 instructions) new-orient-loc location-tracker))))
     ; Return the distance
     (+ (Math/abs x) (Math/abs y))))
 
@@ -49,6 +67,7 @@
   []
   (->> (calculate-distance 
          (read-instructions (file (resource "input.txt"))) ; Instructions
-         ["N" 0 0]) ; Initial Orientation and Location
-       prn))
+         ["N" 0 0] ; Initial Orientation and Location
+         (atom {})) ; Initial Location Tracker
+       prn)) 
 
