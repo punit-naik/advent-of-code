03cbb43775621aa3f956b820eeb4c765e6f5eeed
Punit Naik <naik.punit44@gmail.com>
2017-11-14 10:10:22 +0530

    day-2-v2

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -6,10 +6,22 @@
   "Presses the keypad based upon the direction provided"
   [[current-x current-y] direction]
   (condp = direction
-    \R [current-x (if (< current-y 2) (inc current-y) current-y)]
-    \L [current-x (if (> current-y 0) (dec current-y) current-y)]
-    \D [(if (< current-x 2) (inc current-x) current-x) current-y]
-    \U [(if (> current-x 0) (dec current-x) current-x) current-y]))
+    \R [current-x (cond
+                    (or (zero? current-x) (= current-x 4)) current-y
+                    (or (= current-x 1) (= current-x 3)) (if (< current-y 3) (inc current-y) current-y)
+                    (= current-x 2) (if (< current-y 4) (inc current-y) current-y))]
+    \L [current-x (cond
+                    (or (zero? current-x) (= current-x 4)) current-y
+                    (or (= current-x 1) (= current-x 3)) (if (> current-y 1) (dec current-y) current-y)
+                    (= current-x 2) (if (> current-y 0) (dec current-y) current-y))]
+    \D [(cond
+          (or (zero? current-y) (= current-y 4)) current-x
+          (or (= current-y 1) (= current-y 3)) (if (< current-x 3) (inc current-x) current-x)
+          (= current-y 2) (if (< current-x 4) (inc current-x) current-x)) current-y]
+    \U [(cond
+          (or (zero? current-y) (= current-y 4)) current-x
+          (or (= current-y 1) (= current-y 3)) (if (> current-x 1) (dec current-x) current-x)
+          (= current-y 2) (if (> current-x 0) (dec current-x) current-x)) current-y]))
     
 (defn split-direction
   "Splits directions from a string"
@@ -26,15 +38,19 @@
   "Converts 2-D points into keypad numbers (between 1 and 9)"
   [p]
   (condp = p
-    [0 0] "1"
-    [0 1] "2"
-    [0 2] "3"
-    [1 0] "4"
-    [1 1] "5"
-    [1 2] "6"
-    [2 0] "7"
-    [2 1] "8"
-    [2 2] "9"))
+    [0 2] "1"
+    [1 1] "2"
+    [1 2] "3"
+    [1 3] "4"
+    [2 0] "5"
+    [2 1] "6"
+    [2 2] "7"
+    [2 3] "8"
+    [2 4] "9"
+    [3 1] "A"
+    [3 2] "B"
+    [3 3] "C"
+    [4 2] "D"))
 
 (defn deduce-code
   "Deduces code using instructions"
